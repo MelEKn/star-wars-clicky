a4f48e17bbc92c7db89e7a62c1f28277c2d32251
Melissa Knapp <melknapp@gmail.com>
2019-12-19 15:11:12 -0500

    Updates

---
+++ precache-manifest.ad952494f576c38b3cb3925aab2dfe22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619c4dee5ff1ef5c82c91c046e8cabc7",
+    "url": "/star-wars-clicky-game/index.html"
+  },
+  {
+    "revision": "6c76fa27748ef96a5956",
+    "url": "/star-wars-clicky-game/static/css/main.1c548483.chunk.css"
+  },
+  {
+    "revision": "f98f037de6cfadec3748",
+    "url": "/star-wars-clicky-game/static/js/2.adf09205.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/star-wars-clicky-game/static/js/2.adf09205.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c76fa27748ef96a5956",
+    "url": "/star-wars-clicky-game/static/js/main.277f75a2.chunk.js"
+  },
+  {
+    "revision": "2a6197a216ab1dcd793b",
+    "url": "/star-wars-clicky-game/static/js/runtime-main.0a6f6ea4.js"
+  }
+]);
